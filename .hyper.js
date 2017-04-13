f8a14c6876d996517cdc811aa4d1cb2e457e2392
Michael Minton <michaelcminton@gmail.com>
2017-04-12 22:17:33 -0400

    Create .hyper.js

---
+++ .hyper.js
@@ -0,0 +1,10 @@
+    shell: 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe',
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['-NoLogo'],
+    
+  plugins: [
+	"hyperpower",
+	"hyperborder"
+	],
